ad1e6b2529a4d80644c996d1730667c1b208cd18
slot181 <1253403835@qq.com>
2025-06-07 16:24:25 +0000

    update

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+from astrbot.api import logger, LLMResponse
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
+
+@register("tool_call_notifier", "PluginDeveloper", "一个LLM工具调用消息提示插件", "1.0.0")
+class ToolCallNotifierPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
-    async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.on_llm_response()
+    async def on_tool_call_notifier(self, event: AstrMessageEvent, resp: LLMResponse):
+        """当LLM响应是工具调用时，发送通知"""
+        if resp.role == "tool" and resp.tools_call_name:
+            for tool_name in resp.tools_call_name:
+                logger.info(f"检测到工具调用: {tool_name}")
+                message = f"正在调用 {tool_name} 工具中……"
+                result = event.plain_result(message)
+                await event.send(result)
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        logger.info("工具调用通知插件已卸载。")
